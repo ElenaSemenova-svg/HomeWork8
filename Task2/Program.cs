85cc77998f1c90d8a684a3e3f6a23da96691ba66
Elena Semenova <leo110274@yandex.ru>
2023-02-26 20:12:28 +0300

    add Task1-5 HomeWork8

---
+++ Task2/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[4, 4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RowMinSumElements(int[,] matrix)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            sumRow += matrix[i, j];
+        }
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"Строка {minSumRow + 1} с наименьшей суммой элементов");
+}
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+Console.WriteLine();
+RowMinSumElements(matrix);
---
+++ Task5/Program.cs
@@ -0,0 +1,88 @@
+﻿//  Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int GetNumber(string message)
+{
+int result = 0;
+while (true)
+{
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out result) && result >= 0)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число! Повтороите ввод");
+    } 
+}
+return result;
+}
+
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    int counter = 1;
+    int startColumn = 0;
+    int endColumn = columns - 1;
+    int startRow = 0;
+    int endRow = rows - 1;
+    while (startColumn <= endColumn && startRow <= endRow)
+    {
+        for (int i = startColumn; i <= endColumn; i++)
+        {
+            matrix[startRow, i] = counter;
+            counter++;
+        }
+        startRow++;
+        for (int j = startRow; j <= endRow; j++)
+        {
+          matrix[j, endColumn] = counter;
+          counter++;  
+        }
+        endColumn--;
+        for (int k = endColumn; k >= startColumn; k--)
+        {
+            matrix[endRow, k] =counter;
+            counter++;
+        }
+        endRow --;
+        for (int l = endRow; l >= startRow; l--)
+        {
+            matrix[l, startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+{
+            if (matrix[i, j] < 10)
+            {
+                Console.Write("0" + matrix[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int rows = GetNumber(" Введите количество строк матрицы:");
+int columns = GetNumber(" Введите количество столбцов матрицы:");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+Console.WriteLine();
